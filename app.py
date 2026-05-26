40927ae9b876646d145d0f9e4659ec57b0a1992c
chatshero8-afk <chatshero8@gmail.com>
2026-05-26 11:32:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/send-whatsapp', methods=['POST'])
+@app.route('/send-whatsapp', methods=['POST', 'OPTIONS'])
 def send_whatsapp():
+    if request.method == 'OPTIONS':
+        resp = app.make_default_options_response()
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        return resp
     data = request.get_json()
     resp = requests.post(
         f"{data['api_domain']}/send/text",
         headers={"token": data['token'], "Content-Type": "application/json"},
         json={"to": data['to'], "text": data['text']}
     )
-    return jsonify(resp.json()), resp.status_code
+    response = jsonify(resp.json())
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response, resp.status_code
 
 if __name__ == '__main__':
     app.run()
